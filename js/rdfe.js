6262b08cda451f611a5e47818f736b2faa419dd9
Sebastian Trueg <trueg@kde.org>
2014-12-10 11:05:21 +0100

    Fixed js indentation.

--- js/rdfe.js
+++ js/rdfe.js
@@ -18,12 +18,12 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
-*/
-String.prototype.format = function () {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function (match, number) {
-    return typeof args[number] != 'undefined' ? args[number] : match;
-  });
+ */
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
 };
 
 RDFE = {};
@@ -36,173 +36,189 @@ RDFE.IO_DELETE = 'DELETE DATA {GRAPH <{0}> { <{1}> <{2}> {3} . }}';
 // GSP statements
 RDFE.GSP_RETRIEVE = 'SELECT * WHERE {GRAPH <{0}> { ?s ?p ?o. }}';
 
-RDFE.params = function (params, options)
-{
-  if (!options && !params) return {};
+RDFE.params = function(params, options) {
+    if (!options && !params) return {};
 
-  if (!options) return params;
+    if (!options) return params;
 
-  if (!params) return options;
+    if (!params) return options;
 
-	for (var p in options) {
-	  if (!params[p])
-	    params[p] = options[p];
-  }
+    for (var p in options) {
+        if (!params[p])
+            params[p] = options[p];
+    }
 
-  return params;
+    return params;
 }
 
-RDFE.fileName = function (path)
-{
-  return path.split("/").pop();
+RDFE.fileName = function(path) {
+    return path.split("/").pop();
 }
 
-RDFE.fileParent = function (path)
-{
-  return path.substring(0, path.lastIndexOf('/'));
+RDFE.fileParent = function(path) {
+    return path.substring(0, path.lastIndexOf('/'));
 }
 
-RDFE.io = function (options) {
-	var self = this;
-	this.options = {
-		"async": true
-	}
-	for (var p in options) { this.options[p] = options[p]; }
+RDFE.io = function(options) {
+    var self = this;
+    this.options = {
+        "async": true
+    }
+    for (var p in options) {
+        this.options[p] = options[p];
+    }
 
-	this.retrieve = function (params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec(RDFE.IO_RETRIEVE.format(params.graph), params);
-  }
+    this.retrieve = function(params) {
+        params = RDFE.params(params, this.options);
+        this.exec(RDFE.IO_RETRIEVE.format(params.graph), params);
+    }
 
-	this.insert = function (s, p, o, params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec(RDFE.IO_INSERT.format(params.graph, s, p, o), params);
-  }
+    this.insert = function(s, p, o, params) {
+        params = RDFE.params(params, this.options);
+        this.exec(RDFE.IO_INSERT.format(params.graph, s, p, o), params);
+    }
 
-	this.delete = function (s, p, o, params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec(RDFE.IO_DELETE.format(params.graph, s, p, o), params);
-	}
+    this.delete = function(s, p, o, params) {
+        params = RDFE.params(params, this.options);
+        this.exec(RDFE.IO_DELETE.format(params.graph, s, p, o), params);
+    }
 
-	this.exec = function (q, params) {
-    $(document).ajaxError(params.ajaxError);
-    $(document).ajaxSuccess(params.ajaxSuccess);
+    this.exec = function(q, params) {
+        $(document).ajaxError(params.ajaxError);
+        $(document).ajaxSuccess(params.ajaxSuccess);
 
-    $.ajax({
-      url:  params.host,
-      success: params.success,
-      type: 'GET',
-      async: params.async,
-      data: {"query": q, "format": params.format},
-      dataType: 'text'
-    });
-  }
+        $.ajax({
+            url: params.host,
+            success: params.success,
+            type: 'GET',
+            async: params.async,
+            data: {
+                "query": q,
+                "format": params.format
+            },
+            dataType: 'text'
+        });
+    }
 }
 
-RDFE.gsp = function (options) {
-	var self = this;
-	this.options = {
-		"async": true,
-    "contentType": 'application/octet-stream',
-    "processData": false,
-	}
-	for (var p in options) { this.options[p] = options[p]; }
+RDFE.gsp = function(options) {
+    var self = this;
+    this.options = {
+        "async": true,
+        "contentType": 'application/octet-stream',
+        "processData": false,
+    }
+    for (var p in options) {
+        this.options[p] = options[p];
+    }
 
-	this.retrieve = function (params) {
-	  params = RDFE.params(params, this.options);
-    $(document).ajaxError(params.ajaxError);
-    $(document).ajaxSuccess(params.ajaxSuccess);
+    this.retrieve = function(params) {
+        params = RDFE.params(params, this.options);
+        $(document).ajaxError(params.ajaxError);
+        $(document).ajaxSuccess(params.ajaxSuccess);
 
-    $.ajax({
-      url:  params.host,
-      success: params.success,
-      type: 'GET',
-      async: params.async,
-      data: {"query": RDFE.GSP_RETRIEVE.format(params.graph), "format": params.format},
-      dataType: 'text'
-    });
-  }
+        $.ajax({
+            url: params.host,
+            success: params.success,
+            type: 'GET',
+            async: params.async,
+            data: {
+                "query": RDFE.GSP_RETRIEVE.format(params.graph),
+                "format": params.format
+            },
+            dataType: 'text'
+        });
+    }
 
-	this.insert = function (content, params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec('PUT', content, params);
-  }
+    this.insert = function(content, params) {
+        params = RDFE.params(params, this.options);
+        this.exec('PUT', content, params);
+    }
 
-	this.update = function (content, params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec('POST', content, params);
-	}
+    this.update = function(content, params) {
+        params = RDFE.params(params, this.options);
+        this.exec('POST', content, params);
+    }
 
-	this.delete = function (content, params) {
-	  params = RDFE.params(params, this.options);
-	  this.exec('DELETE', null, params);
-	}
+    this.delete = function(content, params) {
+        params = RDFE.params(params, this.options);
+        this.exec('DELETE', null, params);
+    }
 
-	this.exec = function (method, content, params) {
-    $(document).ajaxError(params.ajaxError);
-    $(document).ajaxSuccess(params.ajaxSuccess);
+    this.exec = function(method, content, params) {
+        $(document).ajaxError(params.ajaxError);
+        $(document).ajaxSuccess(params.ajaxSuccess);
 
-	  var host = params.host + '?graph=' + encodeURIComponent(params.graph);
-    $.ajax({
-      url:  host,
-      success: params.success,
-      type: method,
-      async: params.async,
-      contentType: params.contentType,
-      processData: params.processData,
-      data: content,
-      dataType: 'text'
-    });
-  }
+        var host = params.host + '?graph=' + encodeURIComponent(params.graph);
+        $.ajax({
+            url: host,
+            success: params.success,
+            type: method,
+            async: params.async,
+            contentType: params.contentType,
+            processData: params.processData,
+            data: content,
+            dataType: 'text'
+        });
+    }
 }
 
 RDFE.LDP_INSERT = 'INSERT DATA {GRAPH <{0}> { <{1}> <{2}> {3} . }}';
-RDFE.ldp = function (options) {
-	var self = this;
-	this.options = {
-		"async": true,
-    "dataType": 'text'
-	}
-	for (var p in options) { this.options[p] = options[p]; }
+RDFE.ldp = function(options) {
+    var self = this;
+    this.options = {
+        "async": true,
+        "dataType": 'text'
+    }
+    for (var p in options) {
+        this.options[p] = options[p];
+    }
 
-	this.retrieve = function (path, params) {
-	  params = RDFE.params(params, this.options);
-	  var headers = {"Accept": 'text/turtle, */*;q=0.1'};
-    this.exec('GET', path, headers, null, params);
-  }
+    this.retrieve = function(path, params) {
+        params = RDFE.params(params, this.options);
+        var headers = {
+            "Accept": 'text/turtle, */*;q=0.1'
+        };
+        this.exec('GET', path, headers, null, params);
+    }
 
-	this.insert = function (path, content, params) {
-	  params = RDFE.params(params, this.options);
-    var headers = {"Content-Type": 'text/turtle', "Slug": RDFE.fileName(path)};
-	  this.exec('POST', RDFE.fileParent(path), headers, content, params);
-  }
+    this.insert = function(path, content, params) {
+        params = RDFE.params(params, this.options);
+        var headers = {
+            "Content-Type": 'text/turtle',
+            "Slug": RDFE.fileName(path)
+        };
+        this.exec('POST', RDFE.fileParent(path), headers, content, params);
+    }
 
-	this.update = function (path, s, p, o, params) {
-	  params = RDFE.params(params, this.options);
-	  var content = q.format(RDFE.LDP_INSERT, s, p, o);
-    var headers = {"Content-Type": 'application/sparql-update'};
-	  this.exec('PATCH', path, headers, content, params);
-	}
+    this.update = function(path, s, p, o, params) {
+        params = RDFE.params(params, this.options);
+        var content = q.format(RDFE.LDP_INSERT, s, p, o);
+        var headers = {
+            "Content-Type": 'application/sparql-update'
+        };
+        this.exec('PATCH', path, headers, content, params);
+    }
 
-	this.delete = function (path, params) {
-	  params = RDFE.params(params, this.options);
-    this.exec('DELETE', path, null, null, params);
-	}
+    this.delete = function(path, params) {
+        params = RDFE.params(params, this.options);
+        this.exec('DELETE', path, null, null, params);
+    }
 
-	this.exec = function (method, path, headers, content, params) {
-    $(document).ajaxError(params.ajaxError);
-    $(document).ajaxSuccess(params.ajaxSuccess);
+    this.exec = function(method, path, headers, content, params) {
+        $(document).ajaxError(params.ajaxError);
+        $(document).ajaxSuccess(params.ajaxSuccess);
 
-    $.ajax({
-      url: path,
-      success: params.success,
-      type: method,
-      async: params.async,
-      headers: headers,
-      contentType: 'application/octet-stream',
-      processData: false,
-      data: content,
-      dataType: params.dataType
-    });
-  }
+        $.ajax({
+            url: path,
+            success: params.success,
+            type: method,
+            async: params.async,
+            headers: headers,
+            contentType: 'application/octet-stream',
+            processData: false,
+            data: content,
+            dataType: params.dataType
+        });
+    }
 }
