6eadf3d5ac3378426f604238b8b9f57061305093
Tim Haynes <trueg@openlinksw.com>
2014-12-10 16:45:12 +0000

    Implemented rudimentary triple-identification API

--- js/rdf-editor-api.js
+++ js/rdf-editor-api.js
@@ -13,16 +13,39 @@ function escapeHTML(str) {
 
 // rdf_store bits
 
-function io_index_to_triple_old(i) {
+function io_make_triple(s, p, o) {
+  ss=store.rdf.createNamedNode(store.rdf.resolve(s));
+  pp=store.rdf.createNamedNode(store.rdf.resolve(p));
   
+  if(o[0]=="<") {
+    oo=store.rdf.createNamedNode(store.rdf.resolve(o));
+  } else {
+    oo=store.rdf.createLiteral(o);
+  }
+                                  
+  return(store.rdf.createTriple(ss, pp, oo));
+}
+
+function io_index_to_triple_old(i) {
+//   Return old values of a triple by index i, prior to having been edited
+  var el=$("#sparqlcontents").children("tr[data-statement-index="+i+"]");
+  var s=unescape(el.attr("data-statement-s-old"));
+  var p=unescape(el.attr("data-statement-p-old"));
+  var o=unescape(el.attr("data-statement-o-old"));
+  return(io_make_triple(s,p,o));
 }
 
 function io_index_to_triple(i) {
+//   Return new values of a triple by index i, after having been edited
+  var el=$("#sparqlcontents").children("tr[data-statement-index="+i+"]");
+  var s=el.children("td[data-title='Subject']").text();
+  var p=el.children("td[data-title='Predicate']").text();
+  var o=el.children("td[data-title='Object']").text();
+  return(io_make_triple(s,p,o));
 }
 
-
 function io_draw_graph_contents() {
-  var store=rdfstore.create();
+//   Retrieve a graph via SPARQL construct query and render HTML table
   var host= $("#io_spq").val();
   var graph=encodeURIComponent($("#io_g").val());
   var queryurl=host + '?default-graph-uri=' + graph + '&query=construct+%7B+%3Fs+%3Fp+%3Fo+%7D++WHERE+%7B%3Fs+%3Fp+%3Fo%7D&should-sponge=&format=text%2Fturtle&timeout=30000000';
