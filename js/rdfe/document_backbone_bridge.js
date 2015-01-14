13dc923ea80045310dedc3e9f9be04c60b7ef64c
Sebastian Trueg <trueg@kde.org>
2015-01-14 15:10:43 +0100

    Properly handle rdf nodes in the Backbone-Document bridge.

--- js/rdfe/document_backbone_bridge.js
+++ js/rdfe/document_backbone_bridge.js
@@ -9,7 +9,7 @@ RDFE.Document.Model = Backbone.Model.extend({
 
   addTriple: function(triple) {
     var d = this.get(triple.p.value) || [];
-    d.push(triple.o.value); // FIXME: eventually we will want to include the type and language for decent editors
+    d.push(triple.o);
     this.set(triple.p.value, d);
   },
 
@@ -34,15 +34,13 @@ RDFE.Document.Model = Backbone.Model.extend({
             self.schema[r[i].p.value] = {
               type: "List", //FIXME: convert the code from Aziz into a function which can be reused here, ideally we should use the property's range
               title: r[i].p.value.split(/[/#]/).pop(),
-              itemType: "Text",
-              "node-token": r[i].o.token, // FIXME: eventually these need to come from the ontology instead
-              "node-type": r[i].o.type
+              itemType: "Rdfnode"
             };
             self.fields.push(r[i].p.value);
           }
         }
 
-        // seatch for entity class
+        // search for entity class
         for (var i = 0; i < l; i++) {
           if (!uriClass && (r[i].p.value == RDFE.uriDenormalize('rdf:type')))
             uriClass = r[i].o.value
@@ -87,24 +85,17 @@ RDFE.Document.Model = Backbone.Model.extend({
         if (!val)
           continue;
 
-        var token = self.schema[prop]["node-token"];
-        if (val.constructor !== Array) {
-          if (token == 'uri')
-            val = val.split(',');
-          else
-            val = [val];
-        }
+
+        if (val.constructor !== Array)
+          val = [val];
 
         for (var j = 0; j < val.length; j++) {
-          triples.push(self.doc.store.rdf.createTriple(
-            self.doc.store.rdf.createNamedNode(self.uri),
-            self.doc.store.rdf.createNamedNode(prop),
-            self.doc.store.termToNode({
-              value: val[j],
-              "token": token,
-              type: self.schema[prop]["node-type"]
-            }) // FIXME: eventually we get the token and type and lang from the editor
-          ));
+          var node = val[j].toStoreNode(self.doc.store);
+          if(node.nominalValue.length > 0)
+            triples.push(self.doc.store.rdf.createTriple(
+              self.doc.store.rdf.createNamedNode(self.uri),
+              self.doc.store.rdf.createNamedNode(prop),
+              node));
         }
       }
 
