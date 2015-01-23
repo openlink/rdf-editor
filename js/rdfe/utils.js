c396656a6ee7b1e0decc3e8ba35c603eb3602b88
Dimitar Dimitrov <ddimitrov@openlinksw.com>
2015-01-23 09:53:01 +0100

    Fixed Bug 16558: Better labels in the entity editor.

---
+++ js/rdfe/utils.js
@@ -0,0 +1,38 @@
+if(!window.RDFE)
+  window.RDFE = {};
+
+/*
+ * RDFE Utility functions
+ */
+RDFE.Utils = {};
+
+RDFE.Utils.createTitle = function (str) {
+  if (str) {
+    // replace '_' with space first
+    str = str.replace(/_/g, ' ');
+
+    // strip leading spaces
+    var out = str.replace(/^\s*/, "");
+    out = out.replace(/^[a-z]|[^\s][A-Z]/g, function(str, offset) {
+      if (offset == 0) {
+        return(str.toUpperCase());
+      } else {
+        return(str.substr(0,1) + " " + str.substr(1).toUpperCase());
+      }
+    });
+    return(out);
+  }
+  return str;
+}
+
+RDFE.Utils.escapeXml = function (str) {
+  return str.replace(/[<>&'"]/g, function (c) {
+    switch (c) {
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '\'': return '&apos;';
+      case '"': return '&quot;';
+    }
+  });
+}
