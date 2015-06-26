1258b13ce89fb014d3a0b27d5e310ba08cabd9c1
Dimitar Dimitrov <ddimitrov@openlinksw.com>
2015-06-26 14:11:44 +0200

    Fixed ontology panel view and ontology add behaviour. Added 'Refresh' action.

--- src/ui/ontologyview.js
+++ src/ui/ontologyview.js
@@ -40,7 +40,7 @@
         "sortName": 'prefix',
         "showHeader": false,
         "classes": 'table-hover table-condensed',
-        "height": 100,
+        "height": 157,
         "data": el,
         "idField": 'uri',
         "columns": [{
@@ -63,12 +63,18 @@
           "clickToSelect": false,
           "formatter": function(value, row, index) {
             return [
+              '<a class="refresh ml10" href="javascript:void(0)" title="Refresh">',
+              '  <i class="glyphicon glyphicon-refresh"></i>',
+              '</a>',
               '<a class="remove ml10" href="javascript:void(0)" title="Remove">',
               '  <i class="glyphicon glyphicon-remove"></i>',
               '</a>'
             ].join('');
           },
           "events": {
+            'click .refresh': function(e, value, ontology, index) {
+              self.ontologyManager.parseOntologyFile(ontology.URI, {"ioType": 'http'});
+            },
             'click .remove': function(e, value, ontology, index) {
               self.ontologyManager.ontologyRemove(ontology.URI);
               self.table.bootstrapTable('remove', {
@@ -86,7 +92,7 @@
       // form
       $form.html(
         '<div class="panel panel-default" style="border: 0; padding: 0; margin-bottom: 0;"> ' +
-        '  <div class="panel-body"><div class="form-horizontal"> ' +
+        '  <div class="panel-body" style="padding-top: 10px; padding-bottom: 0;"><div class="form-horizontal"> ' +
         '    <div class="form-group"> ' +
         '      <label for="ontology" class="col-sm-2 control-label">Prefix</label> ' +
         '      <div class="col-sm-10"> ' +
@@ -103,6 +109,7 @@
         '      <div class="col-sm-10 col-sm-offset-2"> ' +
         '        <a href="#" class="btn btn-default cancel">Cancel</a> ' +
         '        <a href="#" class="btn btn-primary ok">OK</a> ' +
+        '        <img src="images/loading.gif" class="loading" style="display: none;"/> ' +
         '      </div> ' +
         '    </div> ' +
         '  </div> ' +
@@ -110,16 +117,64 @@
       );
       $form.find('.cancel').click(function (e) {
         e.preventDefault();
-        self.formContainer.hide();
-        self.tableContainer.show();
-      });
-      $form.find('.ok').click(function (e) {
-        e.preventDefault();
-        self.ontologyManager.parseOntologyFile(self.formContainer.find('#uri').val(), {});
+        var $loading = $form.find('.loading');
+        if ($form.find('.loading').is(":visible"))
+          return;
 
         self.formContainer.hide();
         self.tableContainer.show();
       });
+
+      $form.find('.ok').click(function (e) {
+        e.preventDefault();
+
+        var $loading = $form.find('.loading');
+        if ($loading.is(":visible"))
+          return;
+
+        var formClose = function () {
+          self.formContainer.hide();
+          self.tableContainer.show();
+          $loading.hide();
+        };
+
+        var params = {
+          "success":  function () {
+            $(self.ontologyManager).trigger('loadingFinished', [self.ontologyManager]);
+            formClose();
+          },
+
+          "error": function (state) {
+            $(self.ontologyManager).trigger('loadingFailed', [self.ontologyManager]);
+            var message = (state && state.message)? state.message: 'Error loading ontology';
+            bootbox.confirm(message + '. Do you want an empty ontology to be added?', function(result) {
+              if (result) {
+                var uri = self.formContainer.find('#uri').val();
+                var ontology = self.ontologyManager.ontologyByURI(uri);
+                if (!ontology) {
+                  ontology = new RDFE.Ontology(self.ontologyManager, uri);
+                  var prefix = self.formContainer.find('#prefix').val();
+                  if (prefix) {
+                    ontology.prefix = prefix
+                  }
+                  self.addOntologies(self.ontologyManager.allOntologies());
+                }
+              }
+            });
+            formClose();
+          }
+        };
+        var uri = self.formContainer.find('#uri').val();
+        var ontology = self.ontologyManager.ontologyByURI(uri);
+        if (ontology) {
+          bootbox.alert('This ontology is loaded yet.');
+          return;
+        }
+
+        $loading.show();
+        self.ontologyManager.parseOntologyFile(self.formContainer.find('#uri').val(), params);
+      });
+
       $form.find('#prefix').blur(function (e) {
         var uri = RDFE.ontologyByPrefix(self.formContainer.find('#prefix').val());
         if (uri) {
